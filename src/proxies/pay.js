37793b119a81a61b86e12391a701c6c089fead86
cohena100 <cohena100@gmail.com>
2019-06-21 12:42:10 +0300

    hmm

--- src/proxies/pay.js
+++ src/proxies/pay.js
@@ -2,12 +2,13 @@ const sk = process.env.STRIPE_SECRET_KEY;
 const stripe = require('stripe')(sk);
 
 const create = async (data) => {
+  console.log(JSON.stringify(data));
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
     line_items: [{
       name: 'T-shirt',
       description: 'Comfortable cotton t-shirt',
-      amount: 1500,
+      amount: data.amount,
       currency: 'usd',
       quantity: 1,
       metadata: {
@@ -18,6 +19,7 @@ const create = async (data) => {
     success_url: 'https://stormy-dusk-75310.herokuapp.com/payments/success',
     cancel_url: 'https://stormy-dusk-75310.herokuapp.com/payments/cancel',
   });
+  console.log(JSON.stringify(session));
   return {
     id: session.id,
   };
