1df2427502d990721c5b25455500a563a09a1441
cohena100 <cohena100@gmail.com>
2019-06-21 13:03:00 +0300

    hmm

--- src/proxies/pay.js
+++ src/proxies/pay.js
@@ -2,9 +2,6 @@ const sk = process.env.STRIPE_SECRET_KEY;
 const stripe = require('stripe')(sk);
 
 const create = async (data) => {
-  console.log('avi');
-  console.log(sk);
-  console.log(JSON.stringify(data));
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
     line_items: [{
@@ -14,10 +11,13 @@ const create = async (data) => {
       currency: 'usd',
       quantity: 1,
     }],
+    metadata: {
+      'userId': data.userId,
+      'paymentId': data.paymentId,
+    },
     success_url: 'https://stormy-dusk-75310.herokuapp.com/payments/success',
     cancel_url: 'https://stormy-dusk-75310.herokuapp.com/payments/cancel',
   });
-  console.log(JSON.stringify(session));
   return {
     id: session.id,
   };
